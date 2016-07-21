dcdd62a200e040fd8e814b16fa0a4843953d3161
Brian Grant and Matt Wilkin <brian@>
2016-07-21 10:08:25 -0700

    add tag spec for many recipes.

---
+++ spec/tag_spec.rb
@@ -0,0 +1,13 @@
+require('spec_helper')
+
+describe(Tag) do
+  it('has many recipes') do
+    test_recipe1 = Recipe.create({:name => "Pot Roast"})
+    test_recipe2 = Recipe.create({:name => "Greek Salad"})
+    test_tag = Tag.create({:name => 'Entree'})
+    test_tag.recipes.push(test_recipe1)
+    test_tag.recipes.push(test_recipe2)
+    expect(test_tag.recipes()).to(eq([test_recipe1, test_recipe2]))
+  end
+
+end
