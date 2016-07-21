3449728cf445a9953329b9cde030a198a9ed2691
Brian Grant and Matt Wilkin <brian@>
2016-07-21 10:00:22 -0700

    add tags table, recipe-tags join table, tag Class, many-to-many relationship recipes to tags, and passing spec for tags for a recipe.

--- spec/recipe_spec.rb
+++ spec/recipe_spec.rb
@@ -9,4 +9,13 @@ describe(Recipe) do
     test_recipe.ingredients.push(test_ingredient2)
     expect(test_recipe.ingredients()).to(eq([test_ingredient, test_ingredient2]))
   end
+
+  it('has many tags') do
+    test_recipe = Recipe.create({:name => "Pot Roast"})
+    test_tag = Tag.create({:name => 'Entree'})
+    test_tag2 = Tag.create({:name => 'American'})
+    test_recipe.tags.push(test_tag)
+    test_recipe.tags.push(test_tag2)
+    expect(test_recipe.tags()).to(eq([test_tag, test_tag2]))
+  end
 end
