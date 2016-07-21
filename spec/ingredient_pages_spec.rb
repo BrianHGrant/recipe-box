846974b41f42c0519fc5e83c6e46295e1bfdc1dd
Brian Grant and Matt Wilkin <matt@>
2016-07-21 09:41:45 -0700

    add an integration spec for Ingredient#update.

--- spec/ingredient_pages_spec.rb
+++ spec/ingredient_pages_spec.rb
@@ -1,1 +1,23 @@
 require('spec_helper')
+
+describe('ingredient functionality through application', {:type => :feature}) do
+  it('allows a user to add a new ingredient') do
+    visit('/')
+    click_link('Ingredient view')
+    fill_in('ingredient_name', :with => 'alfalfa sprouts')
+    click_button('Save')
+    expect(page).to have_content('alfalfa sprouts')
+  end
+  it('allows a user to update an ingredient name') do
+    visit('/')
+    click_link('Ingredient view')
+    fill_in('ingredient_name', :with => 'alfalfa sprouts')
+    click_button('Save')
+    click_link('alfalfa sprouts')
+    click_button('Update')
+    fill_in('new_ingredient_name', :with => "blue alfalfa sprouts")
+    click_button('Update Ingredient')
+    expect(page).to have_content('blue alfalfa sprouts')
+  end
+
+end
